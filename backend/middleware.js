0571220d064e501c49f92de11ad60002a19ffc91
udayempire <kuday0726@gmail.com>
2024-07-03 22:51:17 +0530

    added user authentication

---
+++ backend/middleware.js
@@ -0,0 +1,24 @@
+const {JWT_Secret} = require("./config")
+const jwt = require("jsonwebtoken")
+
+const authMiddleWare = (req,res,next)=>{
+    const authHeader = req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith("Bearer")){
+        res.status(401).json({
+            message:"Incorrect token or Bearer missing Authorization"
+        })
+    }
+    const jwtToken = authHeader.split(" ")[1];
+    try{
+        const decodedValue = jwt.verify(jwtToken,JWT_Secret);
+        req.userId = decodedValue.userId;
+        next()
+    }
+    catch(err){
+        return res.status(403).json({
+            message:"Signin again"
+    })
+    }
+}
+
+module.exports= authMiddleWare
