c7858a7c0952ed110719cc68cead8b7ef3a851ef
udayempire <kuday0726@gmail.com>
2024-07-04 19:47:17 +0530

    added some stuffs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-
+import{BrowserRouter,Route,Routes} from "react-router-dom";
+import{Signup} from "./pages/signup"
+import{Signin} from "./pages/signin"
+import{Dashboard} from "./pages/dashboard"
+import{Send} from "./pages/send"
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/signin" element={<Signin/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/send" element={<Send/>}/>
+        </Routes>
+        </BrowserRouter>
+    </>
   )
 }
 
