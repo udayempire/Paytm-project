d4df1da7fb185b1ce3ba3315458ff4f377975448
udayempire <kuday0726@gmail.com>
2024-07-04 13:34:08 +0530

    added accounRouter

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const userRouter = require("./user")
 const router = express.Router();
+const accountRouter = require("./account")
+
 
 router.use("/user",userRouter );
+router.use("/account",accountRouter );
 
 module.exports = router;
