5c680e781c82deef4394a25300c2686b62e12894
udayempire <kuday0726@gmail.com>
2024-07-08 23:15:49 +0530

    nothing

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,8 +1,6 @@
-// import useSearchParams from "react-router-dom"
 import {useState,useEffect} from "react"
 import axios from "axios"
 export const Balance=()=> {
-    // const searchParam = useSearchParams()
     const [balance,setBalance] = useState("")
     useEffect(()=>{
         axios.get("http://localhost:3000/api/v1/account/balance",{
